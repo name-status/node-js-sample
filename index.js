7a795515bd541827d9399c374448fc41acaeffd6
name-status <martin.korvas@gmail.com>
2018-09-25 18:42:27 +0200

    Added custom response.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('node-js-sample running on port 5000')
 })
 
 app.listen(app.get('port'), function() {
